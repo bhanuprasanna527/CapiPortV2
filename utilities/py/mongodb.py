4cd0338ca85bbe1e2a2616c1f6611ac54b95975c
Bhanu Prasanna <63473951+bhanuprasanna2001@users.noreply.github.com>
2024-03-21 20:15:11 +0100

    Update

---
+++ utilities/py/mongodb.py
@@ -0,0 +1,29 @@
+from pymongo import MongoClient
+import socket
+from dotenv import load_dotenv
+import os
+import urllib
+
+load_dotenv()
+def mongodb_push_data (company_name_to_symbol, number_of_symbols):
+    username = urllib.parse.quote_plus(os.environ.get("USERNAME"))
+    password = urllib.parse.quote_plus(os.environ.get("PASSWORD"))
+
+    MONGO_DB_URI = "mongodb+srv://%s:%s@capiport.xtnx5it.mongodb.net/" % (username, password)
+
+    client = MongoClient (MONGO_DB_URI)
+    if "capiport" in client. list_database_names():
+        db = client ["capiport"]
+    else:
+        db = client ["capiport"]
+    collection = db ["stocks they picked"]
+    hostname = socket.gethostname()
+    ip_address = socket. gethostbyname(hostname)
+    data = {
+        "hostname": hostname,
+        "stocks_picked": company_name_to_symbol,
+        'number_of_stocks_picked': number_of_symbols,
+        "ip_address": ip_address
+    }
+    collection. insert_one(data)
+    client. close ()
